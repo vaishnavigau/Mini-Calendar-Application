9976d6a4947be3987b9e87d46fbabe635ef7e2de
Vaishnavi Gautam <139965479+vaishnavigau@users.noreply.github.com>
2026-02-23 10:51:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateCalendar() {
+    const now = new Date();
+    const day = now.getDate();
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[now.getMonth()];
+    document.getElementById('day').textContent = day;
+    document.getElementById('month').textContent = month;
+}
+
+updateCalendar();
